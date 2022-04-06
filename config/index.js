c987086d7f1e32644d47c7ecf8f0b818496dfe0a
muzigaiyu <muzigaiyu@outlook.com>
2022-04-06 09:01:08 +0800

    init

---
+++ config/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  build: {
+    env: require('./prod.env'),
+    DisableF12: true,
+    cleanConsole: true,
+    openDevTools: false,
+  },
+  dev: {
+    env: require('./dev.env'),
+    removeElectronJunk: true,
+    chineseLog: false,
+    port: 9080
+  },
+  development: require('./dev.env'),
+  production: require('./prod.env'),
+  UseStartupChart: true,
+  IsUseSysTitle: true,
+  DllFolder: '',
+  BuiltInServerPort: 25565
+}
